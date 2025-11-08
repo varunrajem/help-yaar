e40df4cc9d5eb78a3d76d04e5b7757e511f173ce
Varunraje <varunraje47@gmail.com>
2025-11-08 15:21:32 +0530

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import { useState } from 'react'
-import './App.css'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
 import { Navbar } from './components/Navbar'
 import { Footer } from './components/Footer'
-import Login from './pages/Login'
-import Signup from './pages/Signup'
-import About from './pages/About'
+
+// Lazy imports
+const Home = lazy(() => import('./pages/Home'))
+const About = lazy(() => import('./pages/About'))
+const Login = lazy(() => import('./pages/Login'))
+const Signup = lazy(() => import('./pages/Signup'))
 
 function App() {
   return (
-    <>
-      <Router>
-        <Navbar />
+    <Router>
+      <Navbar />
+      {/* Suspense shows a loading message while the page loads */}
+      <Suspense fallback={<div className="text-center mt-10">Loading...</div>}>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/aboutus' element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/aboutus" element={<About />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
         </Routes>
-        <Footer />
-      </Router>
-    </>
+      </Suspense>
+      <Footer />
+    </Router>
   )
 }
 
