5f0531695e83ca369d1f90dddc2831de8efbb210
Varunraje <varunraje47@gmail.com>
2025-11-08 09:49:48 +0530

    route and pages created for login, singup

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import { Navbar } from './components/Navbar'
+import { Footer } from './components/Footer'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className='bg-green-500'>dfg</div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/signup' element={<Signup />} />
+        </Routes>
+        <Footer />
+      </Router>
     </>
   )
 }
---
+++ src/pages/Signup.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div className='bg-red-500'>Signup</div>
+  )
+}
+
+export default Signup
